ea24a365f7cb09087bf31ad62663279ecf370b6c
Badru Ndawula <badrundawula24@gmail.com>
2023-12-06 10:42:04 +0300

    add querychanges

--- queries.sql
+++ queries.sql
@@ -1,81 +1,45 @@
 /*Queries that provide answers to the questions from all projects.*/
+-- Queries using JOIN to answer the questions
+-- Add these queries to your queries.sql file
 
-SELECT * FROM animals WHERE name LIKE '%mon';
+-- 1. What animals belong to Melody Pond?
+SELECT * FROM animals
+JOIN owners ON animals.owner_id = owners.id
+WHERE owners.full_name = 'Melody Pond';
 
-SELECT name FROM animals WHERE EXTRACT(YEAR FROM date_of_birth) BETWEEN 2016 AND 2019;
+-- 2. List of all animals that are Pokemon.
+SELECT * FROM animals
+JOIN species ON animals.species_id = species.id
+WHERE species.name = 'Pokemon';
 
-SELECT name FROM animals WHERE neutered = true AND escape_attempts < 3;
+-- 3. List all owners and their animals, including those that don't own any animal.
+SELECT owners.full_name, animals.name AS animal_name
+FROM owners
+LEFT JOIN animals ON owners.id = animals.owner_id;
 
-SELECT date_of_birth FROM animals WHERE name IN ('Agumon', 'Pikachu');
-
-SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
-
-SELECT * FROM animals WHERE neutered = true;
-
-SELECT * FROM animals WHERE name != 'Gabumon';
-
-SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
-
-SELECT COUNT(*) FROM animals;
-
-SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
-
-SELECT AVG(weight_kg) FROM animals;
-
-SELECT neutered, MAX(escape_attempts) FROM animals GROUP BY neutered;
-
-SELECT species, MIN(weight_kg), MAX(weight_kg) FROM animals GROUP BY species;
-
-SELECT species, AVG(escape_attempts)
+-- 4. How many animals are there per species?
+SELECT species.name, COUNT(*) AS animal_count
 FROM animals
-WHERE EXTRACT(YEAR FROM date_of_birth) BETWEEN 1990 AND 2000
-GROUP BY species;
+JOIN species ON animals.species_id = species.id
+GROUP BY species.name;
 
--- Tranaction 1
-BEGIN;
+-- 5. List all Digimon owned by Jennifer Orwell.
+SELECT animals.name
+FROM animals
+JOIN owners ON animals.owner_id = owners.id
+JOIN species ON animals.species_id = species.id
+WHERE owners.full_name = 'Jennifer Orwell' AND species.name = 'Digimon';
 
-UPDATE animals SET species = 'unspecified';
+-- 6. List all animals owned by Dean Winchester that haven't tried to escape.
+SELECT animals.name
+FROM animals
+JOIN owners ON animals.owner_id = owners.id
+WHERE owners.full_name = 'Dean Winchester' AND animals.escape_attempts = 0;
 
-SELECT * FROM animals;
-
-ROLLBACK;
-
-SELECT * FROM animals;
-
--- Tranaction 2
-BEGIN;
-UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
-
-UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
-
-SELECT * FROM animals;
-
-COMMIT;
-
-SELECT * FROM animals;
-
--- Tranaction 3
-BEGIN;
-DELETE FROM animals;
-
-ROLLBACK;
-
-SELECT * FROM animals;
-
--- Transaction 4
-BEGIN;
-
-DELETE FROM animals WHERE date_of_birth > '2022-01-01';
-
-SAVEPOINT my_savepoint;
-
-UPDATE animals SET weight_kg = weight_kg * -1;
-
-
-ROLLBACK TO my_savepoint;
-
-UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
-
-COMMIT;
-
-SELECT * FROM animals;
+-- 7. Who owns the most animals?
+SELECT owners.full_name, COUNT(*) AS animal_count
+FROM owners
+LEFT JOIN animals ON owners.id = animals.owner_id
+GROUP BY owners.full_name
+ORDER BY animal_count DESC
+LIMIT 1;
